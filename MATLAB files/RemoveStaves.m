97389b3c0aa765cfd4d74e0b2c64e58cc6db5134
Hazem AbuMostafa <hbluebrince@gmail.com>
2014-12-16 18:20:01 +0200

    Removing Staves: More return values.
    - RemoveStaves now returns the start and end points of staves in the
    X-axis (respectively, after the image).
    - Some fixes and renaming.

--- MATLAB files/RemoveStaves.m
+++ MATLAB files/RemoveStaves.m
@@ -1,9 +1,9 @@
-function newImg = RemoveStaves(img)
+function [newImg,xStart,xEnd] = RemoveStaves(img)
     img = rgb2gray(img);
-    img = im2double(img);
+    img = im2double(img); %Work on the double form (MATLAB compatibility issues).
     
     %Thresholding:
-    img = im2bw(img, 0.75); %graythresh(img) / multithresh(img)
+    img = im2bw(img, 0.75); %others: graythresh(img) / multithresh(img).
     
     %Get size:
     [m,n] = size(img);
@@ -12,35 +12,37 @@ function newImg = RemoveStaves(img)
     gram = uint32(zeros(m,1));
     for i = 1:m
        for j = 1:n
-          if img(i,j)==0 %<=200
+          if img(i,j)==0 %old yaw: <=200
              gram(i) = gram(i)+1;
           end
        end
     end
     
-    %Testing:
+    %Applying:
     peakMax = max(max(gram));
-    newImg = imcomplement(img);
-    widthStart = 0;
-    widthEnd = 0;
+    newImg = imcomplement(img); %Work on the complement, to make the background black.
+    xStart = 0;
+    xEnd = 0;
     
     for i = 1:m
        theSum = sum(newImg(i,:));
        if theSum >= (peakMax*0.75) && theSum <= (0.98*n)
            for j = 1:n
-               if i>1 && newImg(i,j)==1 && newImg(i-1,j)==0 %Stave confirmed
-                   newImg(i,j) = 0;     %A pixel part of a stave; delete it.
-                   if widthStart==0
-                       widthStart = j;  %The start pixel of all staves in Y axis
-                   elseif j>widthEnd
-                       widthEnd = j;
+               if i>1 && newImg(i,j)==1 && newImg(i-1,j)==0 %Stave confirmed.
+                   newImg(i,j) = 0; %A pixel part of a stave; delete it.
+                   if xStart==0
+                       xStart = j; %The start pixel of all staves in X axis.
+                   elseif j>xEnd
+                       xEnd = j; %The end pixel of all staves in X axis.
                    end
                end
            end
        end
     end
     
-    newImg = imcomplement(newImg);
-    newImg(:,widthStart) = 0;
-    newImg(:,widthEnd) = 0;
+    newImg = imcomplement(newImg); %Back to original colors.
+    
+    %Testing X start and end points:
+    %newImg(:,widthStart) = 0;
+    %newImg(:,widthEnd) = 0;
 end
