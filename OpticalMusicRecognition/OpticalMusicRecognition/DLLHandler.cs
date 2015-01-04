b1ff1329911da88f4f2aae49694d4697496c47d8
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-01-04 08:58:09 +0200

    DLL's & C# Linking Project

---
+++ OpticalMusicRecognition/OpticalMusicRecognition/DLLHandler.cs
@@ -0,0 +1,75 @@
+﻿using MathWorks.MATLAB.NET.Arrays;
+using OMRLibrary;
+using System.Drawing;
+
+//change PkgName to Project Package
+//change DLLCLASS to exported class in DLL
+//change FunctionName to the intiating function
+namespace OMRLink
+{
+    class DLLHandler
+    {
+        private OMRClass OMRHandler;
+        
+        public DLLHandler()
+        {
+            OMRHandler = new OMRClass();
+        }
+
+        public void PlaySheet(Bitmap inp)
+        {
+            double[][,] im = getArr(inp);
+            OMRHandler.OMR((MWNumericArray)im[0]);
+
+            // Create the MATLAB instance 
+            //MLApp.MLApp matlab = new MLApp.MLApp();
+
+            // Change to the directory where the function is located 
+            //matlab.Execute(@"cd D:\GitHub\ImageProcessingCollegeApplication\MATLAB files");
+
+            // Define the output 
+            //object result = null;
+
+            // Call the MATLAB function myfunc
+            //matlab.Feval("OMR", 7, out result, (MWNumericArray)im[0]);
+        }
+       
+        private static double[][,] getArr(Bitmap inp)
+        {
+            double[][,] img = new double[3][,];
+            img[0] = new double[inp.Width, inp.Height];
+            img[1] = new double[inp.Width, inp.Height];
+            img[2] = new double[inp.Width, inp.Height];
+            for (int i = 0; i < inp.Width; i++)
+                for (int j = 0; j < inp.Height; j++)
+                {
+                    Color temp = inp.GetPixel(i, j);
+                    img[0][i, j] = temp.R;
+                    img[1][i, j] = temp.G;
+                    img[2][i, j] = temp.B;
+                }
+            return img;
+        }
+        
+        private static Bitmap getImg(Bitmap inp, double[,] arr, double[,] arr1, double[,] arr2)
+        {
+            Bitmap img = new Bitmap(inp);
+            for (int i = 0; i < inp.Width; i++)
+                for (int j = 0; j < inp.Height; j++)
+                {
+                    img.SetPixel(i, j, Color.FromArgb((int)arr[i, j], (int)arr1[i, j], (int)arr2[i, j]));
+                }
+            return img;
+        }
+        private static Bitmap getImg(Bitmap inp, byte[,] arr, byte[,] arr1, byte[,] arr2)
+        {
+            Bitmap img = new Bitmap(inp);
+            for (int i = 0; i < inp.Width; i++)
+                for (int j = 0; j < inp.Height; j++)
+                {
+                    img.SetPixel(i, j, Color.FromArgb((int)arr[i, j], (int)arr1[i, j], (int)arr2[i, j]));
+                }
+            return img;
+        }
+    }
+}
---
+++ OpticalMusicRecognition/OpticalMusicRecognition/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OMRLink
+{
+    public partial class MainForm : Form
+    {
+        List<string> supported = new List<string> { "png", "jpg", "bmp", "tif", "tiff", "gif", "ppm" };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap openImage(String filePath){
+            string extension = Path.GetExtension(filePath).ToLower();
+            extension = extension.Substring(1, extension.Length-1);
+
+            Bitmap bitmap = null;
+            if(supported.Contains(extension))
+                bitmap = new Bitmap(filePath);
+            else
+                MessageBox.Show("This type of files is not supported... yet.\n(Are you sure it's an image?)",
+                    "Not Supported", MessageBoxButtons.OK);
+            return bitmap;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if(pictureBox.Image != null)
+            {
+                DLLHandler handler = new DLLHandler();
+                handler.PlaySheet(pictureBox.Image as Bitmap);
+            }
+        }
+
+        private void pictureBox_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if(fileDialog.ShowDialog() == DialogResult.OK){
+                Bitmap bitmap = openImage(fileDialog.FileName);
+                pictureBox.Image = bitmap;
+            }
+        }
+    }
+}
